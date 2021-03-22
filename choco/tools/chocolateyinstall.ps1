26a4dff5677aa22dd954452e84f756714e60610e
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-03-22 11:20:23 +0100

    Created choco/tools/chocolateyinstall.ps1

---
+++ choco/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿# Variabiles
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ppath = "C:\Progra~1\agentless"
+$exepath = "$ppath\agentless.exe"
+echo "My personal path is: $ppath"
+echo "My exe path id $exepath"
+
+# Set folder
+New-Item -ItemType Directory -Force -Path $ppath
+Copy-Item "$toolsDir\agentless.exe" "$ppath\agentless.exe"
+
+# Add Path
+$new_path = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
+if ($new_path -notlike "*$ppath*")
+{
+	$new_path += ";$ppath"
+	$new_path = $new_path.Replace(";;", ";")
+	Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $new_path
+}
+
+# Installing pypi
+pip.exe install agentlesscd
